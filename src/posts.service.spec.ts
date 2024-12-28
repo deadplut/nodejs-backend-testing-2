cbf70f76630fd78a829925d49f4f93beafb09100
Suganov Boris <suganov@t8.ru>
2024-12-28 18:22:38 +0300

    finish task

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,29 +1,48 @@
-import { PostsService } from './posts.service';
+import { PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
 
   beforeEach(() => {
     postsService = new PostsService();
   });
 
-  describe('.findMany', () => {
+  describe(".findMany", () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: "Post 1" },
+      { text: "Post 2" },
+      { text: "Post 3" },
+      { text: "Post 4" },
     ];
 
     beforeEach(() => {
       posts.forEach((post) => postsService.create(post));
     });
 
-    it('should return all posts if called without options', () => {
+    it("should return all posts if called without options", () => {
+      const f_posts = postsService.findMany();
+      expect(f_posts).toEqual(expect.arrayContaining(posts));
+
       // реализуйте тест-кейс
     });
 
-    it('should return correct posts for skip and limit options', () => {
+    it("should return correct posts for skip and limit options", () => {
+      const f_posts = postsService.findMany({ skip: 1, limit: 1 });
+      expect(f_posts).toEqual(expect.arrayContaining([posts[1]]));
+      // реализуйте тест-кейс
+    });
+
+    it("should return correct posts only skip", () => {
+      const f_posts = postsService.findMany({ skip: 1 });
+      expect(f_posts).toEqual(
+        expect.arrayContaining([posts[1], posts[2], posts[3]])
+      );
+      // реализуйте тест-кейс
+    });
+
+    it("should return correct posts only limit", () => {
+      const f_posts = postsService.findMany({ limit: 1 });
+      expect(f_posts).toEqual(expect.arrayContaining([posts[0]]));
       // реализуйте тест-кейс
     });
 
